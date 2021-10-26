9886f0b7b7bbe18948ca519ba1b6f242042f3c10
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-10-26 22:37:39 +0530

    12

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://myntrahackathon:myntrahackathon@cluster0.vgomr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
-app.use(express.static("public"));
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.listen(3000, function() {
-    console.log(" Started server on port 3000");
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true ,useUnifiedTopology: true })
+  .then(result => {
+    app.get("/", function(req, res) {
+      res.sendFile(__dirname + "/index.html");
+  })
+  
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
