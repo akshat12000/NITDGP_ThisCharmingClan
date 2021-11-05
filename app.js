86c75c24993717f48f2052f0337802aeada8c0cc
Akshat <akshatbhatnagar174@gmail.com>
2021-11-05 22:19:27 +0530

    allocated the routes and changed the styling of some portion

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const User = require('./models/user');
 const Doctor = require('./models/doctor');
 
 const MONGODB_URI =
-  'mongodb+srv://myntrahackathon:myntrahackathon@cluster0.vgomr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+    'mongodb+srv://myntrahackathon:myntrahackathon@cluster0.vgomr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
+    uri: MONGODB_URI,
+    collection: 'sessions'
 });
 
 app.set('view engine', 'ejs');
@@ -35,174 +35,165 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
 );
 
 const appointmentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Patient's name required"]
-},
-contact: {
-    type: String,
-},
-date: {
-    type: Date,
-    required: [true, "Date of appointment required"]
-},
-time: {
-    type: String,
-    required: [true, "Time of appointment required"]
-},
-symptoms: {
-    type: String,
-},
-status: {
-    type: String,
-    required: true
-},
-  meetLink: {
-      type: String,
-  },
-  patientId: {
-      type: String
-  },
-  doctorId: {
-      type: String
-  },
-  doctorName: {
-      type: String
-  },
-  qualifications: {  
-      type: String
-  }
+    name: {
+        type: String,
+        required: [true, "Patient's name required"]
+    },
+    contact: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        required: [true, "Date of appointment required"]
+    },
+    time: {
+        type: String,
+        required: [true, "Time of appointment required"]
+    },
+    symptoms: {
+        type: String,
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    meetLink: {
+        type: String,
+    },
+    patientId: {
+        type: String
+    },
+    doctorId: {
+        type: String
+    },
+    doctorName: {
+        type: String
+    },
+    qualifications: {
+        type: String
+    }
 });
 
 var Appointment = mongoose.model("Appointment", appointmentSchema);
 
 app.get("/appointment_form", (req, res) => {
-  if(!req.session.isLoggedIn){
-    res.redirect('/login');
-}
-else
-{
-  res.render("doctor/appointment_form",{ isAuthenticated: true});
-}
-  
+    if (!req.session.isLoggedIn) {
+        res.redirect('/login');
+    } else {
+        res.render("doctor/appointment_form", { isAuthenticated: true });
+    }
+
 });
 
 app.get("/appointment_form/status", (req, res) => {
-  const curUser=req.session.user._id;
-  Appointment.find({'patientId':curUser}, (err, appointments) => {
-    res.render('doctor/status',{appointments:appointments,isAuthenticated:true});
+    const curUser = req.session.user._id;
+    Appointment.find({ 'patientId': curUser }, (err, appointments) => {
+        res.render('doctor/status', { appointments: appointments, isAuthenticated: true });
     })
-  }
-        );
-
-        app.post("/appointment_form", (req, res) => {
-          const appointment = new Appointment({
-              name: req.body.Name,
-              contact: req.body.Contact,
-              date: req.body.Date,
-              time: req.body.Time,
-              symptoms: req.body.Symptoms,
-              status: "notAssigned",
-              patientId:req.session.user._id
-          });
-          appointment.save((err) => {
-              if (!err) {
-                  res.redirect("/appointment_form/status");
-              } else {
-                  console.log(err);
-              }
-          });
-      });
-
-      app.post("/delete", (req, res) => {
-        const currentId = req.body.delbtn;
-        
-        Appointment.findByIdAndRemove(currentId, function(err) {
-            if (!err) {
-                console.log("Item Deleted Successfully!");
-                res.redirect("/appointment_form");
-            }
-        });
-    
-    });
-
-      app.get("/doctor_portal", async (req, res) => {
-        
-        if(!req.session.isLoggedIn){
-          res.redirect('/doctor/login');
-      }
-      else
-      {
-        var curUser=req.session.user._id;
-        const curdoc= await Doctor.findOne({_id:curUser});
-
-        if( curdoc==null){
-            res.send("You don't have the permission to access the site")
-        }
-        else
-        {
-          Appointment.find({ }, (err, appointments) => {
-            res.render('doctor/doctor',{appointments:appointments,curUser:curUser, isAuthenticated: true});
-            })
-        }
- 
-               
-      }
-             
-        
-    });  
-
-    app.post("/confirm", async (req, res) => {
-      const docId=req.session.user._id;
-      const doctor= await Doctor.findOne({_id:docId});
-      console.log(doctor);
-      const appointmentId=req.body.cnf;
-      Appointment.updateOne({_id:appointmentId},{meetLink: "https://meet.jit.si/" + appointmentId,status: "assigned",doctorId:docId,doctorName:doctor.name,qualifications:doctor.qualifications}, function (err,app){
-        if(err)
-        {
-          console.log(err)
-        }
-      });
-           res.redirect("/doctor_portal");
-  });
-  app.post("/complete", (req, res) => {
-    const docId=req.session.user._id;
-    const appointmentId=req.body.cnf;
-    Appointment.updateOne({_id:appointmentId},{status: "completed"}, function (err,app){
-      if(err)
-      {
-        console.log(err)
-      }
-    });
-         res.redirect("/doctor_portal");
 });
 
-app.get('/blog', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
+app.post("/appointment_form", (req, res) => {
+    const appointment = new Appointment({
+        name: req.body.Name,
+        contact: req.body.Contact,
+        date: req.body.Date,
+        time: req.body.Time,
+        symptoms: req.body.Symptoms,
+        status: "notAssigned",
+        patientId: req.session.user._id
+    });
+    appointment.save((err) => {
+        if (!err) {
+            res.redirect("/appointment_form/status");
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/delete", (req, res) => {
+    const currentId = req.body.delbtn;
+
+    Appointment.findByIdAndRemove(currentId, function(err) {
+        if (!err) {
+            console.log("Item Deleted Successfully!");
+            res.redirect("/appointment_form");
+        }
+    });
+
+});
+
+app.get("/doctor_portal", async(req, res) => {
+
+    if (!req.session.isLoggedIn) {
+        res.redirect('/doctor/login');
+    } else {
+        var curUser = req.session.user._id;
+        const curdoc = await Doctor.findOne({ _id: curUser });
+
+        if (curdoc == null) {
+            res.send("You don't have the permission to access the site")
+        } else {
+            Appointment.find({}, (err, appointments) => {
+                res.render('doctor/doctor', { appointments: appointments, curUser: curUser, isAuthenticated: true });
+            })
+        }
+
+
+    }
+
+
+});
+
+app.post("/confirm", async(req, res) => {
+    const docId = req.session.user._id;
+    const doctor = await Doctor.findOne({ _id: docId });
+    console.log(doctor);
+    const appointmentId = req.body.cnf;
+    Appointment.updateOne({ _id: appointmentId }, { meetLink: "https://meet.jit.si/" + appointmentId, status: "assigned", doctorId: docId, doctorName: doctor.name, qualifications: doctor.qualifications }, function(err, app) {
+        if (err) {
+            console.log(err)
+        }
+    });
+    res.redirect("/doctor_portal");
+});
+app.post("/complete", (req, res) => {
+    const docId = req.session.user._id;
+    const appointmentId = req.body.cnf;
+    Appointment.updateOne({ _id: appointmentId }, { status: "completed" }, function(err, app) {
+        if (err) {
+            console.log(err)
+        }
+    });
+    res.redirect("/doctor_portal");
+});
+
+app.get('/blog', async(req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
 })
 app.use('/articles', articleRouter)
 
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 
@@ -214,13 +205,12 @@ app.use(dauthRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true ,useUnifiedTopology: true })
-  .then(result => {
-  
-  
-    app.listen(5000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-  
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+
+
+        app.listen(5000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
